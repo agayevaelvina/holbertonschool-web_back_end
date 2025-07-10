a903aaecdf34b6105cf4fcea343ed80e8233bfaf
Your Name <you@example.com>
2025-07-10 16:32:53 +0000

    Commit

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,3 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
-def list_all(mongo_collection):
-    documents= list(mongo_collection.find())
-client =MongoClient("mongodb://localhost:27017/")
-db = client["database1"]
-collection = db["school"]  
-documents = list_all(collection)
-print(documents)
-
+documents = list(mongo_collection.find())
+    return documents if documents else []
