150f66834bd25d379fc4249909973a8ee22afa23
agayevaelvina <elvinaagyeva@gmail.com>
2025-07-11 19:00:57 +0000

    x

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 def update_topics(mongo_collection, name, topics):
-    "Task 10"
+    '''Task 10'''
     mongo_collection.update_many(
-            {"name":name},
-            {"$set":{"topics":topics}})
+        { "name": name },
+        { "$set": { "topics": topics } }
+    )
