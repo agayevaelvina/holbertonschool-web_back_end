aaedcbf783d1e8b2aa582b5bb2c7152a5ded0dbb
agayevaelvina <elvinaagyeva@gmail.com>
2025-07-11 20:17:46 +0000

    x

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-'''x'''
+'''Task 11'''
+
 def schools_by_topic(mongo_collection, topic):
-    """x"""
-    return list(mongo_collection.find({"topics": topic}))
+    '''Task 11'''
+    return list(mongo_collection.find({ "topics": topic }))
